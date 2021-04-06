0802dc304c09184762ad683ec93b1b160ddd8e40
Christian Elías <contacto@christianecg.com>
2021-04-06 16:18:38 -0500

    Add talks

--- src/talks.js
+++ src/talks.js
@@ -11,56 +11,54 @@ const talks_id = `
     width: 100%;
 `;
 
-const talks = `
-    display: grid;
-    grid-template-columns: 50% 50%;
-    margin-top: 3rem;
-`;
-
-const talk = `
-    width: 100%;
-    max-width: 500px;
-    min-width: 100px;
-    margin-bottom: 1rem;
-`;
-
 const video = `
-    width: 95%;
-    height: 300px;
+    width: 400px;
+    height: 40vh;
+    max-width: 100vw;
 `;
 
 function getTalk(title, id_video) {
     return `
-        <article style='${talk}'>
-            <center><h6>${title}</h6></center>
-            <center><iframe
-                style='${video}'
-                src="https://www.youtube.com/embed/${id_video}"
-                frameborder="0"
-                allow="
-                    accelerometer;
-                    autoplay;
-                    clipboard-write;
-                    encrypted-media;
-                    gyroscope;
-                    picture-in-picture"
-                allowfullscreen
-            >
-            </iframe></center>
+        <article class='slide'>
+            <center>
+                <h6>${title}</h6><br>
+                <iframe
+                    style='${video}'
+                    src="https://www.youtube.com/embed/${id_video}"
+                    frameborder="0"
+                    allow="
+                        accelerometer;
+                        autoplay;
+                        clipboard-write;
+                        encrypted-media;
+                        gyroscope;
+                        picture-in-picture"
+                    allowfullscreen
+                >
+                </iframe>
+            </center>
         </article>
     `
+}
+
+function getTalks() {
+    let talks = `
+        ${getTalk('Juguetes para hackers y makers','cuGrSembV8s')}
+        ${getTalk('Importancia del diseño en el desarrollo web','ANvHZzmmnss')}
+        ${getTalk('Análisis de la pandemia Covid-19 en México aplicando modelos de M.L.','mF4yEaIo_Bw')}
+    `
+
+    return talks + talks;
 }
 
 export default function getTalksSection() {
     return `
         <section id='talks' style='${talks_id}'>
             <a href='#about'><p class='arrow__up arrow__white'></p></a>
-            <h3>Charlas cool que he dado<br></h3>
-            <section style='${talks}'>
-                ${getTalk('Juguetes para hackers y makers','cuGrSembV8s')}
-                ${getTalk('Importancia del diseño en el desarrollo web','ANvHZzmmnss')}
-                ${getTalk('Análisis de la pandemia Covid-19 en México aplicando modelos de M.L.','mF4yEaIo_Bw')}
-            </section>
+            <h4 style='margin-bottom: 1rem'>Charlas cool que he dado</h4>
+            <section class='slider'><section class='slide-track'>
+                ${getTalks()}
+            </section></section>
             <a href='#talks'><p class='arrow__down arrow__white'></p></a>
         </section>
     `;
