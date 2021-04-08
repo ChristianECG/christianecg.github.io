f88bfcd7561e2c5919898f123718f00191e75b62
Christian Elías <contacto@christianecg.com>
2021-04-07 22:37:26 -0500

    Fix talks section

--- src/talks.js
+++ src/talks.js
@@ -11,54 +11,38 @@ const talks_id = `
     width: 100%;
 `;
 
-const video = `
-    width: 400px;
-    height: 40vh;
-    max-width: 100vw;
+const link = `
+    color: #f5f5f5;
+    text-decoration: none;
+    margin-bottom: 1rem;
 `;
 
 function getTalk(title, id_video) {
     return `
-        <article class='slide'>
-            <center>
-                <h6>${title}</h6><br>
-                <iframe
-                    style='${video}'
-                    src="https://www.youtube.com/embed/${id_video}"
-                    frameborder="0"
-                    allow="
-                        accelerometer;
-                        autoplay;
-                        clipboard-write;
-                        encrypted-media;
-                        gyroscope;
-                        picture-in-picture"
-                    allowfullscreen
-                >
-                </iframe>
-            </center>
-        </article>
+        <a
+            href='https://youtu.be/${id_video}'
+            target='_blank'
+            style='${link}'
+            class='talk'>
+            &#8227; ${title}
+        </a>
     `
 }
 
 function getTalks() {
-    let talks = `
+    return `
         ${getTalk('Juguetes para hackers y makers','cuGrSembV8s')}
-        ${getTalk('Importancia del diseño en el desarrollo web','ANvHZzmmnss')}
         ${getTalk('Análisis de la pandemia Covid-19 en México aplicando modelos de M.L.','mF4yEaIo_Bw')}
+        ${getTalk('Importancia del diseño en el desarrollo web','ANvHZzmmnss')}
     `
-
-    return talks + talks;
 }
 
 export default function getTalksSection() {
     return `
         <section id='talks' style='${talks_id}'>
             <a href='#about'><p class='arrow__up arrow__white'></p></a>
-            <h4 style='margin-bottom: 1rem'>Charlas cool que he dado</h4>
-            <section class='slider'><section class='slide-track'>
-                ${getTalks()}
-            </section></section>
+            <h4 style='margin-bottom: 1rem;'>Charlas cool que he dado</h4>
+            ${getTalks()}
             <a href='#talks'><p class='arrow__down arrow__white'></p></a>
         </section>
     `;
